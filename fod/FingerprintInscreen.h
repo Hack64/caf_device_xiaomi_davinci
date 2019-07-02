f0808683415b9020556cc04559ed42e0aa15b07f
LuK1337 <priv.luk@gmail.com>
2019-07-02 15:05:43 +0200

    davinci: fod: Implement handleAcquire()
    Change-Id: Ica796fbd468e2df0ee666a0d0c6cbde628d6989f
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -56,6 +56,8 @@ public:
 
 private:
     sp<IXiaomiFingerprint> xiaomiFingerprintService;
+    std::mutex mCallbackLock;
+    sp<IFingerprintInscreenCallback> mCallback;
     android::base::unique_fd touch_fd_;
 };
 
