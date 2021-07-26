d60ac21a878eaf67e1a172874976c5ecc7eb26c7
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-07-26 10:56:30 +0200

    Revert "davinci: Move pa specific overlays into overlay-pa and remove lineage specific overlays"
    This reverts commit 80eebbb548dd755a4375f4c4831918ebe337b7a7.

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/overlay-lineage
 
 # Sensors
 PRODUCT_PACKAGES += \
